21c3d08ef2ab6429ca117149a0b1dab249be2d8f
bristeele99 <bristeele99@gmail.com>
2023-12-13 14:33:34 -0500

    react save

---
+++ reactweek8/reactweek8/src/components/Article.js
@@ -0,0 +1,30 @@
+
+
+export default function Article () {
+
+return(
+<main>
+<article>
+  <h4 class="blog-date"></h4>
+  <img
+  class="family-photo"
+  alt="photo"
+  src="blog-image-1.jpg"
+  /> 
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In aliquam sem fringilla ut morbi tincidunt augue interdum velit. Amet consectetur adipiscing elit duis tristique sollicitudin. Senectus et netus et malesuada fames ac turpis egestas sed. Eget aliquet nibh praesent tristique magna sit amet. Sed arcu non odio euismod lacinia at quis. Ac auctor augue mauris augue neque gravida in fermentum. Donec ultrices tincidunt arcu non sodales neque sodales ut. Sed cras ornare arcu dui vivamus arcu felis. Duis at consectetur lorem donec massa sapien faucibus. Et pharetra pharetra massa massa. Malesuada proin libero nunc consequat interdum varius sit amet. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Eu tincidunt tortor aliquam nulla facilisi. Nibh sed pulvinar proin gravida. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Praesent elementum facilisis leo vel fringilla est. Quam vulputate dignissim suspendisse in est ante in.</p>
+  <a>Continues...</a>
+</article>
+<article>
+  <h4 class="blog-date"></h4>
+  <img
+  class="photo"
+  alt="photo"
+  src="blog-image-2.jpg"
+  />
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In aliquam sem fringilla ut morbi tincidunt augue interdum velit. Amet consectetur adipiscing elit duis tristique sollicitudin. Senectus et netus et malesuada fames ac turpis egestas sed. Eget aliquet nibh praesent tristique magna sit amet. Sed arcu non odio euismod lacinia at quis. Ac auctor augue mauris augue neque gravida in fermentum. Donec ultrices tincidunt arcu non sodales neque sodales ut. Sed cras ornare arcu dui vivamus arcu felis. Duis at consectetur lorem donec massa sapien faucibus. Et pharetra pharetra massa massa. Malesuada proin libero nunc consequat interdum varius sit amet. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Eu tincidunt tortor aliquam nulla facilisi. Nibh sed pulvinar proin gravida. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Praesent elementum facilisis leo vel fringilla est. Quam vulputate dignissim suspendisse in est ante in.</p>
+  <a>Continues...</a>
+</article>
+</main>
+)
+}
+
---
+++ reactweek8/reactweek8/src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav'
+
+export default function Article () {
+    return(
+        <header>
+        <h1>Sartre's List</h1>
+        <h2>Better-Dressed People</h2>
+        <Nav/>
+        </header>
+
+    )
+}
